64571cc199f493c0a4f4eb08c1d540419662378a
Kuba Turek <ja@kubaturek.pl>
2017-03-12 16:08:25 +0100

    Create Validator interface

---
+++ src/Validator/Validator.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+namespace EPS\PhpUri\Validator;
+
+interface Validator
+{
+    /**
+     * Determines whether given string is a valid URI or not.
+     * Returns true when URI can be parsed, false when URI is invalid.
+     *
+     * @param string $uriCandidate
+     * @return bool
+     */
+    public function validate(string $uriCandidate): bool;
+}
