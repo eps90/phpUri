aa919f892380cf2183e19753fc503a9d541ddfc2
Kuba Turek <ja@kubaturek.pl>
2017-03-14 17:52:22 +0100

    Add one more test case for Rfc3986Parser tests

--- tests/Parser/Rfc3986ParserTest.php
+++ tests/Parser/Rfc3986ParserTest.php
@@ -6,6 +6,8 @@ namespace EPS\PhpUri\Tests\Parser;
 use EPS\PhpUri\Exception\ParserException;
 use EPS\PhpUri\Parser\Parser;
 use EPS\PhpUri\Parser\Rfc3986Parser;
+use EPS\PhpUri\Uri;
+use EPS\PhpUri\UriAuthority;
 
 class Rfc3986ParserTest extends AbstractParserTest
 {
@@ -23,5 +25,21 @@ class Rfc3986ParserTest extends AbstractParserTest
 
         $invalidUri = 'http';
         $this->parser->parseUri($invalidUri);
+    }
+
+    /**
+     * @test
+     * @see https://tools.ietf.org/html/rfc3986#section-3.2
+     */
+    public function itShouldParseUriWithEmptyAuthority()
+    {
+        $validUriString = 'http://';
+        $expectedUri = new Uri(
+            'http',
+            new UriAuthority()
+        );
+        $actualUri = $this->parser->parseUri($validUriString);
+
+        static::assertEquals($expectedUri, $actualUri);
     }
 }
